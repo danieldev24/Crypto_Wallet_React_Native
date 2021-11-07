3b78d9b532604c9f71e6e3a3761b9d2f49d4f8f3
HoangSon <sonlth@neoconsulting.com.vn>
2021-11-07 17:35:11 +0700

    complete market screen

--- screens/Market.js
+++ screens/Market.js
@@ -8,6 +8,7 @@ import {
   Image,
 } from "react-native";
 import { MainLayout } from "./";
+import { LineChart } from "react-native-chart-kit";
 import { connect } from "react-redux";
 import { useFocusEffect } from "@react-navigation/native";
 import { getCoinMarket } from "../stores/market/marketAction";
@@ -19,13 +20,67 @@ const marketTabs = constants.marketTabs.map((marketTab) => ({
   ref: React.createRef(),
 }));
 
-const Tabs = () => {
+const TabIndicator = ({ measureLayout, scrollX }) => {
+  const inputRange = marketTabs.map((_, i) => i * SIZES.width);
+  const translateX = scrollX.interpolate({
+    inputRange,
+    outputRange: measureLayout.map((measure) => measure.x),
+  });
+  return (
+    <Animated.View
+      style={{
+        position: "absolute",
+        left: 0,
+        height: "100%",
+        width: (SIZES.width - SIZES.radius * 2) / 2,
+        borderRadius: SIZES.radius,
+        backgroundColor: COLORS.lightGray,
+        transform: [
+          {
+            translateX,
+          },
+        ],
+      }}
+    />
+  );
+};
+
+const Tabs = ({ scrollX, onMarketTabPress }) => {
+  const [measureLayout, setMeasureLayout] = React.useState([]);
+  const containerRef = React.useRef();
+
+  React.useEffect(() => {
+    let ml = [];
+
+    marketTabs.forEach((marketTab) => {
+      marketTab?.ref?.current?.measureLayout(
+        containerRef.current,
+        (x, y, width, height) => {
+          ml.push({
+            x,
+            y,
+            width,
+            height,
+          });
+
+          if (ml.length === marketTabs.length) {
+            setMeasureLayout(ml);
+          }
+        }
+      );
+    });
+  }, [containerRef.current]);
   return (
     <View
+      ref={containerRef}
       style={{
         flexDirection: "row",
       }}
     >
+      {/* Tab indicator */}
+      {measureLayout.length > 0 && (
+        <TabIndicator measureLayout={measureLayout} scrollX={scrollX} />
+      )}
       {/* Tab */}
       {marketTabs.map((item, index) => {
         return (
@@ -34,6 +89,7 @@ const Tabs = () => {
             style={{
               flex: 1,
             }}
+            onPress={() => onMarketTabPress(index)}
           >
             <View
               ref={item.ref}
@@ -59,55 +115,234 @@ const Tabs = () => {
     </View>
   );
 };
-function renderTabBars() {
-  return (
-    <View
-      style={{
-        marginTop: SIZES.radius,
-        marginHorizontal: SIZES.radius,
-        borderRadius: SIZES.radius,
-        backgroundColor: COLORS.gray,
-      }}
-    >
-      <Tabs />
-    </View>
-  );
-}
-
-function renderButons() {
-  return (
-    <View
-      style={{
-        flexDirection: "row",
-        marginTop: SIZES.radius,
-        marginHorizontal: SIZES.radius,
-      }}
-    >
-      <TextButton label="USD" />
-
-      <TextButton
-        label="% 7d"
-        containerStyle={{
-          marginLeft: SIZES.base,
-        }}
-      />
-
-      <TextButton
-        label="Top"
-        containerStyle={{
-          marginLeft: SIZES.base,
-        }}
-      />
-    </View>
-  );
-}
 
 const Market = ({ getCoinMarket, coins }) => {
+  const scrollX = React.useRef(new Animated.Value(0)).current;
+  const marketTabScrollViewRef = React.useRef();
+
+  const onMarketTabPress = React.useCallback((index) => {
+    marketTabScrollViewRef?.current?.scrollToOffset({
+      offset: index * SIZES.width,
+    });
+  });
   useFocusEffect(
     React.useCallback(() => {
       getCoinMarket();
     }, [])
   );
+  function renderTabBars() {
+    return (
+      <View
+        style={{
+          marginTop: SIZES.radius,
+          marginHorizontal: SIZES.radius,
+          borderRadius: SIZES.radius,
+          backgroundColor: COLORS.gray,
+        }}
+      >
+        <Tabs scrollX={scrollX} onMarketTabPress={onMarketTabPress} />
+      </View>
+    );
+  }
+
+  function renderButons() {
+    return (
+      <View
+        style={{
+          flexDirection: "row",
+          marginTop: SIZES.radius,
+          marginHorizontal: SIZES.radius,
+        }}
+      >
+        <TextButton label="USD" />
+
+        <TextButton
+          label="% 7d"
+          containerStyle={{
+            marginLeft: SIZES.base,
+          }}
+        />
+
+        <TextButton
+          label="Top"
+          containerStyle={{
+            marginLeft: SIZES.base,
+          }}
+        />
+      </View>
+    );
+  }
+
+  function renderList() {
+    return (
+      <Animated.FlatList
+        ref={marketTabScrollViewRef}
+        data={marketTabs}
+        contentContainerStyle={{
+          marginTop: SIZES.padding,
+        }}
+        horizontal
+        pagingEnabled
+        scrollEventThrottle={16}
+        snapToAlignment="center"
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(item) => item.id}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+          {
+            useNativeDriver: false,
+          }
+        )}
+        renderItem={({ item, index }) => {
+          return (
+            <View
+              style={{
+                flex: 1,
+                width: SIZES.width,
+              }}
+            >
+              <FlatList
+                data={coins}
+                keyExtractor={(item) => item.id}
+                renderItem={({ item, index }) => {
+                  let priceColor =
+                    item.price_change_percentage_7d_in_currency == 0
+                      ? COLORS.lightGray3
+                      : item.price_change_percentage_7d_in_currency > 0
+                      ? COLORS.lightGreen
+                      : COLORS.red;
+                  return (
+                    <View
+                      style={{
+                        flexDirection: "row",
+                        paddingHorizontal: SIZES.padding,
+                        marginBottom: SIZES.radius,
+                      }}
+                    >
+                      {/* Coins */}
+                      <View
+                        style={{
+                          flex: 1,
+                          flexDirection: "row",
+                          alignItems: "center",
+                        }}
+                      >
+                        <Image
+                          source={{ uri: item.image }}
+                          style={{
+                            height: 20,
+                            width: 20,
+                          }}
+                        />
+
+                        <Text
+                          style={{
+                            color: COLORS.white,
+                            ...FONTS.h3,
+                            marginLeft: SIZES.radius,
+                          }}
+                        >
+                          {item.name}
+                        </Text>
+                      </View>
+
+                      {/* Line Chart */}
+                      <View
+                        style={{
+                          flex: 1,
+                          alignItems: "center",
+                        }}
+                      >
+                        <LineChart
+                          withVerticalLabels={false}
+                          withHorizontalLabels={false}
+                          withDots={false}
+                          withInnerLines={false}
+                          withVerticalLines={false}
+                          withOuterLines={false}
+                          data={{
+                            datasets: [
+                              {
+                                data: item.sparkline_in_7d.price,
+                              },
+                            ],
+                          }}
+                          width={100}
+                          height={60}
+                          chartConfig={{
+                            color: () => priceColor,
+                          }}
+                          bezier
+                          style={{
+                            paddingRight: 0,
+                          }}
+                        />
+                      </View>
+
+                      {/* Figures */}
+                      <View
+                        style={{
+                          flex: 1,
+                          alignItems: "flex-end",
+                          justifyContent: "center",
+                        }}
+                      >
+                        <Text
+                          style={{
+                            color: COLORS.white,
+                            ...FONTS.h4,
+                          }}
+                        >
+                          $ {item.current_price}
+                        </Text>
+
+                        <View
+                          style={{
+                            flex: 1,
+                            alignItems: "flex-end",
+                            justifyContent: "center",
+                          }}
+                        >
+                          {item.price_change_percentage_7d_in_currency != 0 && (
+                            <Image
+                              source={icons.upArrow}
+                              style={{
+                                height: 10,
+                                width: 10,
+                                tintColor: priceColor,
+                                transform:
+                                  item.price_change_percentage_7d_in_currency >
+                                  0
+                                    ? [{ rotate: "45deg" }]
+                                    : [{ rotate: "125deg" }],
+                              }}
+                            />
+                          )}
+                          <Text
+                            style={{
+                              marginLeft: 5,
+                              color: priceColor,
+                              ...FONTS.body5,
+                              lineHeight: 15,
+                            }}
+                          >
+                            {item.price_change_percentage_7d_in_currency.toFixed(
+                              2
+                            )}
+                            %
+                          </Text>
+                        </View>
+                      </View>
+                    </View>
+                  );
+                }}
+              />
+            </View>
+          );
+        }}
+      />
+    );
+  }
   return (
     <MainLayout>
       <View
@@ -126,6 +361,7 @@ const Market = ({ getCoinMarket, coins }) => {
         {renderButons()}
 
         {/* List */}
+        {renderList()}
       </View>
     </MainLayout>
   );
